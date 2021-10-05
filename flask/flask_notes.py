ff603f869cb7bbe0876bcfbd0ecfe861f103c095
TareqJudehGithub <tareq.joudeh@gmail.com>
2021-10-05 21:07:47 +0200

    Installed and configured Docker on my win10 machine.

---
+++ flask/flask_notes.py
@@ -0,0 +1,12 @@
+"""
+Flask
+ What is Flask?
+  Flask is a lightweight WSGI web application framework.
+  Flask official website: https://palletsprojects.com/p/werkzeug/
+
+ What type of websites flask can build?
+  - Complex websites with user protected sensitive data.
+  - API back-end with single page app JS front-end
+  - And many more.
+
+"""
--- flask/sec2_Intro/sec_notes.py
+++
@@ -1,8 +0,0 @@
-"""
-Flask
- What is Flask?
-  Flask is a lightweight WSGI web application framework.
-  Flask official website: https://palletsprojects.com/p/werkzeug/
-
-
-"""
